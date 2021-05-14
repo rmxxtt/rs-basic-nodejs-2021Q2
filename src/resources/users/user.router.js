3851f680afe6550428bad5b7a1420d0a5b917a47
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2021-05-14 23:30:42 +0700

    fix:: added return status codes

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -5,12 +5,12 @@ const usersService = require('./user.service');
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
   // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  res.status(200).json(users.map(User.toResponse));
 });
 
 router.route('/').post(async (req, res) => {
   const user = await usersService.create(req);
-  res.json(User.toResponse(user));
+  res.status(201).json(User.toResponse(user));
 });
 
 router.route('/:id').get(async (req, res) => {
