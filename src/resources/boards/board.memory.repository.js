d5784d34ad18c9847e18ff146c520ae68213f8a0
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2021-05-31 02:44:18 +0700

    feat: delete all comments 3

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -1,16 +1,35 @@
 const { DB } = require('../../database/database');
 const BOARD = require('./board.model');
 
+/**
+ *
+ * @returns {Promise<[]>}
+ */
 const getAll = async () => DB.boards;
 
+/**
+ *
+ * @param req
+ * @returns {Promise<Board>}
+ */
 const create = async (req) => {
   const board = new BOARD(req.body);
   DB.boards.push(board);
   return board;
 };
 
+/**
+ *
+ * @param req
+ * @returns {Promise<*>}
+ */
 const get = async (req) => DB.boards.find(board => board.id === req.params.id);
 
+/**
+ *
+ * @param req
+ * @returns {Promise<boolean|*>}
+ */
 const update = async (req) => {
   const board = await get(req);
   if (board) {
@@ -24,6 +43,12 @@ const update = async (req) => {
   return false;
 };
 
+/**
+ *
+ * @param req
+ * @returns {Promise<boolean>}
+ * @private
+ */
 const delete_ = async (req) => {
   const index = DB.boards.findIndex(board => board.id === req.params.id);
 
